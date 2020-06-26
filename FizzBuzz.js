295de3c97d98de9daa061de5f9f9c7436dd8552f
kylejw2 <kylejamwright@gmail.com>
2020-06-26 10:39:52 -0600

    Initial commit

---
+++ FizzBuzz.js
@@ -0,0 +1,31 @@
+for (let i = 1; i < 100; i++) {
+    let str = "";
+    if (i % 3 === 0) str += "Fizz";
+    if (i % 5 === 0) str += "Buzz";
+    if (str !== "") console.log(str);
+}
+
+let i = 1;
+while (i < 100) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    }
+}
+
+
+const printFizzBuzz = input => {
+    let str = "";
+    if (input % 3 === 0) str += "Fizz";
+    if (input % 5 === 0) str += "Buzz";
+    return str !== "" ? console.log(str) : false;
+}
+
+let j = 1;
+do {
+    printFizzBuzz(j);
+    j++;
+} while (j < 100);
